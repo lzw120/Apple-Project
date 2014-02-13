ac0edd59215cf9b5256dd37d720903a9309e9265
ZewenLi <zewenli@ZewenLis-MacBook-Pro.local>
2014-02-12 23:58:11 -0500

    finished 1st step, built a local map

--- ArrestsPlotter/ArrestsPlotter/ViewController.m
+++ ArrestsPlotter/ArrestsPlotter/ViewController.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2014 ZewenLi. All rights reserved.
 //
 
+#define METERS_PER_MILE 1609.344
+
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -18,6 +20,19 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    // 1
+    CLLocationCoordinate2D zoomLocation;
+    zoomLocation.latitude = 39.281516;
+    zoomLocation.longitude= -76.580806;
+    
+    // 2
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, 0.5*METERS_PER_MILE, 0.5*METERS_PER_MILE);
+    
+    // 3
+    [_mapView setRegion:viewRegion animated:YES];
 }
 
 - (void)didReceiveMemoryWarning
